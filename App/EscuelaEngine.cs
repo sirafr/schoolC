c1b6b6aaed52edda70b090e9619dc507d665d60f
sirafr <andres22f@hotmail.com>
2021-09-20 23:17:10 -0500

    class28 DatosAlumnos

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using CoreEscuela.Entidades;
 
 namespace CoreEscuela
@@ -17,13 +19,57 @@ namespace CoreEscuela
             Escuela = new Escuela("Platzi Academy", 2020, TiposEscuela.Secundaria,
             pais: "Ecuador", ciudad: "Quito");
 
+            CargarCursos();
+            CargarAsignaturas();
+            
+            foreach (var curso in Escuela.Cursos)
+            {
+                curso.Alumnos.AddRange(CargarAlumnos());
+            }
+
+            CargarEvaluaciones();
+        }
+
+        private void CargarEvaluaciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CargarAsignaturas()
+        {
+            foreach (var curso in Escuela.Cursos)
+            {
+                var listaAsignaturas = new List<Asignatura>(){
+                    new Asignatura{Nombre="Matematicas"},
+                    new Asignatura{Nombre="Eduacion fisica"},
+                    new Asignatura{Nombre="Castellano"},
+                    new Asignatura{Nombre="Ciencias Naturales"}
+                };
+                curso.Asignaturas.AddRange(listaAsignaturas);
+            }
+        }
+
+        private IEnumerable<Alumno> CargarAlumnos()
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listoAlumnos = from n1 in nombre1
+                                from n2 in nombre2
+                                from a1 in apellido1
+                                select new Alumno{Nombre=$"{n1} {n2} {a1}"};
+            return listoAlumnos;
+        }
+
+        private void  CargarCursos()
+        {
             Escuela.Cursos = new List<Curso>(){
                         new Curso(){ Nombre = "101", Jornada = TiposJornada.Manana },
                         new Curso() {Nombre = "201", Jornada = TiposJornada.Manana},
                         new Curso{Nombre = "301", Jornada = TiposJornada.Manana},
                         new Curso() {Nombre = "401", Jornada=TiposJornada.Tarde},
                         new Curso() {Nombre = "501", Jornada=TiposJornada.Tarde}
-
             };
         }
     }
